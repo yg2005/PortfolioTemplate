1e6e7cd260a3125ec2f7607467ba5b9f997460ef
yg2005 <yg2005icloud@gmail.com>
2025-01-06 23:06:37 -0500

    description

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Yash! Your`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Financial Analyst", "ML Engineer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>I'm Yash Gudimalla, a sophomore at Rutgers University in New Brunswick, pursuing double majors in Finance and Computer Science. My academic journey revolves around my passion for working with financial data analysis and merging it with software development to drive business solutions. I'm deeply interested in leveraging data-driven approaches to optimize financial strategies and decision-making processes. My goal is to contribute to innovative projects that require a blend of analytical skills and programming expertise! </p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
