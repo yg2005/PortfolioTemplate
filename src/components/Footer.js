8850dc42c3f6995a91075a4c0ebf3dc1aa8af8f5
yg2005 <yg2005icloud@gmail.com>
2025-01-22 23:15:14 -0500

    Certs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon2 from "../assets/img/github.png";
 
 export const Footer = () => {
   return (
@@ -11,16 +10,15 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <MailchimpForm />
-          <Col size={12} sm={6}>
+          <Col size={12} sm={6} className="d-flex align-items-center">
             <img src={logo} alt="Logo" />
+            <div className="social-icon ms-3">
+              <a href="https://www.linkedin.com/in/yg13"><img src={navIcon1} alt="LinkedIn Icon" /></a>
+              <a href="https://github.com/yg2005"><img src={navIcon2} alt="GitHub Icon" /></a>
+            </div>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
-            </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2025. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,14 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
+
 import colorSharp from "../assets/img/color-sharp.png"
+import BMC from "../assets/img/BMC.png";
+import JPMC from "../assets/img/JPMC.png";
+import Acc from "../assets/img/Acc.png";
+import Excel from "../assets/img/Excel.png";
+import UBS from "../assets/img/UBS.png";
+import GenAI from "../assets/img/GenAI.png";
 
 export const Skills = () => {
   const responsive = {
@@ -34,31 +37,55 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Certifications</h2>
-                        <p>Bridging the gap between Computer Science and Finance through innovative solutions.<br></br> Specializing in Web Development, Quantitative Analysis, and Machine Learning.</p>
+                        <p>Exploring the intersection of Computer Science and Finance to create innovative solutions.<br></br> Specializing in Web Development, Quantitative Analysis, and Machine Learning.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Full Stack Development</h5>
+                                <a href={BMC} target="_blank" rel="noopener noreferrer">
+                                    <img className="cert-image" src={BMC} alt="Bloomberg Market Concepts" />
+                                </a>
+                                <a href="https://portal.bloombergforeducation.com/certificates/KXR6uNf7cqTo7MZU26yZ3Pb6" target="_blank" rel="noopener noreferrer">
+                                    <h5 className="cert-text">Bloomberg Market Concepts</h5>
+                                </a>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Quantitative Analysis</h5>
+                                <a href={JPMC} target="_blank" rel="noopener noreferrer">
+                                    <img className="cert-image" src={JPMC} alt="J.P. Morgan Chase" />
+                                </a>
+                                <a href="https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/J.P.%20Morgan/bWqaecPDbYAwSDqJy_JPMorgan%20Chase%20&%20Co._EbY757uKuCuHpdPFD_1724101080173_completion_certificate.pdf" target="_blank" rel="noopener noreferrer">
+                                    <h5 className="cert-text">J.P. Morgan Quant Research</h5>
+                                </a>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Machine Learning</h5>
+                                <a href={Acc} target="_blank" rel="noopener noreferrer">
+                                    <img className="cert-image" src={Acc} alt="Accenture" />
+                                </a>
+                                <a href="https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/T6kdcdKSTfg2aotxT/XvvYRvb2YpfkyA52H_T6kdcdKSTfg2aotxT_EbY757uKuCuHpdPFD_1730830361158_completion_certificate.pdf" target="_blank" rel="noopener noreferrer">
+                                    <h5 className="cert-text">Accenture Software Development</h5>
+                                </a>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Financial Modeling</h5>
+                                <a href={Excel} target="_blank" rel="noopener noreferrer">
+                                    <img className="cert-image" src={Excel} alt="Excel" />
+                                </a>
+                                <a href="https://www.linkedin.com/learning/certificates/2a63fbb7df39390f9189fedd6ec10e556cfc491d60fc3e6c5de86d3732b8b6c0?u=76115650" target="_blank" rel="noopener noreferrer">
+                                    <h5 className="cert-text">Excel Essential Training</h5>
+                                </a>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Algorithm Development</h5>
+                                <a href={UBS} target="_blank" rel="noopener noreferrer">
+                                    <img className="cert-image" src={UBS} alt="UBS" />
+                                </a>
+                                <a href="https://my.amplifyme.com/certificate/d8df8345-8cfe-4789-b715-cc24c0c0ccea" target="_blank" rel="noopener noreferrer">
+                                    <h5 className="cert-text">Morgan Stanley & UBS</h5>
+                                </a>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Data Analysis</h5>
+                                <a href={GenAI} target="_blank" rel="noopener noreferrer">
+                                    <img className="cert-image" src={GenAI} alt="Generative AI" />
+                                </a>
+                                <a href="https://credentials.databricks.com/cdf1e819-31fb-45fd-afcb-f16bb20afc69#acc.vIU9tGWN" target="_blank" rel="noopener noreferrer">
+                                    <h5 className="cert-text">Generative AI</h5>
+                                </a>
                             </div>
                         </Carousel>
                     </div>
